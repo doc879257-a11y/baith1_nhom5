233f3b1b7075ac8089bb41b899000cdd6f9487c4
ka khuyen <kakhuyen2446@gmail.com>
2025-09-10 10:04:59 +0700

    ka khuyen them Bai1.java

---
+++ Bai1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Bai1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhap ten cua ban: ");
+        String ten = sc.nextLine();
+        System.out.println("Hello " + ten);
+        sc.close();
+    }
+}
